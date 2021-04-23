46ab8a4b8858ceb835c7c869030112ef965f841d
Robert Rivera <59786779+Robert288@users.noreply.github.com>
2021-04-22 22:47:47 -0700

    Completed part 2

---
+++ turnin/rrive020_lab5_part2.c
@@ -0,0 +1,135 @@
+/*	Author: Robert Rivera 
+ *	Lab Section: 021
+ *	Assignment: Lab #5  Exercise #1
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum Button_States {Button_Start, Initial, Increment, Decrement, Reset, Wait, Wait1, Wait2} Button_State;
+
+unsigned char incrementButton = 0x00;
+unsigned char decrementButton = 0x00;
+unsigned char tmpC = 0x00;
+
+void TickFct_Button() {
+	switch (Button_State) {
+		case Button_Start:
+			Button_State = Initial;
+			break;
+
+		case Initial:
+			if (incrementButton && decrementButton) {
+				Button_State = Reset;
+			} else if (incrementButton && !decrementButton) {
+				Button_State = Increment;
+			} else if (!incrementButton && decrementButton) {
+				Button_State = Decrement;
+			} else {
+				Button_State = Initial;
+			}
+			break;
+
+		case Increment:
+		case Wait1:
+			if (incrementButton && decrementButton) {
+				Button_State = Reset;
+			} else if (!incrementButton && !decrementButton) {
+				Button_State = Initial;
+			} else if (!incrementButton && decrementButton) {
+				Button_State = Decrement;
+			} else {
+				Button_State = Wait1;
+			}
+			break;
+
+		case Decrement:
+		case Wait2:
+			if (incrementButton && decrementButton) {
+                                Button_State = Reset;
+                        } else if (!incrementButton && !decrementButton) {
+                                Button_State = Initial;
+                        } else if (incrementButton && !decrementButton) {
+                                Button_State = Increment;
+                        } else {
+                                Button_State = Wait2;
+                        }		
+			break;
+		
+		case Reset: 
+                        if (!incrementButton && !decrementButton) {
+                                Button_State = Initial;
+                        } else {
+                                Button_State = Wait;
+                        }
+			break;
+			
+		case Wait: 
+                        if (!incrementButton && !decrementButton) {
+                                Button_State = Initial;
+                        } else if (incrementButton && !decrementButton) {
+                                Button_State = Increment;
+                        } else if (!incrementButton && decrementButton) {
+				Button_State = Decrement;
+			} else {
+                                Button_State = Wait;
+                        }		
+			break;
+	}
+
+	switch (Button_State) {
+		case Initial:
+			break;
+
+		case Increment:
+			if (tmpC < 9) {
+				tmpC = tmpC + 1;
+			}
+			break;
+
+		case Decrement:
+			if (tmpC > 0) {
+				tmpC = tmpC - 1;
+			}
+			break;
+
+		case Reset:
+			tmpC = 0;
+			break;
+
+		case Wait:
+			break;
+
+		case Wait1:
+			break;
+
+		case Wait2:
+			break;
+
+		default:
+			break;
+	}
+}
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+
+	Button_State = Button_Start;
+	tmpC = 0x07;
+
+  while (1) {
+		incrementButton = ~PINA & 0x01;
+		decrementButton = ~PINA & 0x02;
+		decrementButton = (decrementButton >> 1);
+		TickFct_Button();
+		PORTC = tmpC;
+	}
+
+	return 1;
+}
