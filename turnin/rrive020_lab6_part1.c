e9a90eb03b7fd3346c90de94be38417d3ffa0926
Robert Rivera <59786779+Robert288@users.noreply.github.com>
2021-05-02 07:06:06 -0700

    Completed part 1

--- turnin/rrive020_lab6_part1.c
+++ turnin/rrive020_lab6_part1.c
@@ -6,7 +6,7 @@
  * I acknowledge all content contained herein, excluding template or example
  * code is my own original work.
  *
- *  Demo Link:
+ *  Demo Link: https://youtu.be/HXSCzq57_es
  */
 
 #include <avr/io.h>
